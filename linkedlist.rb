1cd743f9696ed30877506b74f3f6bd649ee813e1
CourtneyNicole <courtney_nicole_flynn@yahoo.com>
2019-09-26 01:32:53 +0000

    linkedlist

--- linkedlist.rb
+++ linkedlist.rb
@@ -1,3 +1,13 @@
+class LinkedListNode
+  attr_accessor :value, :next_node
+
+  def initialize(value, next_node=nil)
+    @value = value
+    @next_node = next_node
+  end
+end
+
+
 class Stack
     attr_reader :data
 
@@ -8,6 +18,9 @@ class Stack
     # Push a value onto the stack
     def push(value)
         # IMPLEMENT ME!
+        puts @data.inspect
+        @data = LinkedListNode.new(value, @data)
+        puts @data.inspect
     end
 
     # Pop an item off the stack.
@@ -29,3 +42,26 @@ def reverse_list(list)
 
     # ADD CODE HERE
 end
+
+# Creates a new Stack object
+stack = Stack.new
+
+#Pushes (adds) the number 1 to the empty stack
+stack.push(1)
+stack.push(4)
+
+ # Pushes the number 2 to the TOP of the stack
+stack.push(2)
+
+# # Pops (removes) the TOP number from the stack (2)
+# puts stack.pop
+# => 2
+
+# # Pops the remaining number from the stack (1)
+# puts stack.pop
+# => 1
+
+# # Tries to pop the TOP number from the stack,
+# # but it is empty so it returns nil
+# puts stack.pop
+# => nil
